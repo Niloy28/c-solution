6dcf7a41a651073069af2ceea9e469bc6f6bc85a
Niloy28 <Niloy28@live.com>
2018-01-18 20:34:52 +0600

    Update 15.c

--- Chapter 8/Projects/15.c
+++ Chapter 8/Projects/15.c
@@ -9,8 +9,22 @@ int main(void)
     char msg[SIZE];
     int shift, i;
 
-    printf("Enetr message to be encrypted: ");
+    printf("Enter message to be encrypted: ");
     gets(msg);
     printf("Enter shift amount (1-25): ");
     scanf("%d", &shift);
+
+    printf("Encrypted message: ");
+    for(i = 0; msg[i]; i++){
+        if(isalpha(msg[i])){
+            if(isupper(msg[i]))
+                printf("%c", ((msg[i] - 'A' + shift) % 26) + 'A');
+            else if(islower(msg[i]))
+                printf("%c", ((msg[i] - 'a' + shift) % 26) + 'a');
+        }
+        else printf("%c", msg[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
